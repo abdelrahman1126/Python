e33bbcedb31a1d74ea146d07fb4b7aca88b6b616
abdelrahman1126 <bodorody@gmail.com>
2026-02-25 17:06:55 +0200

    My first project in 24 hours

---
+++ market calculator.py
@@ -0,0 +1,6 @@
+from math import *
+name = input ("اكتب اسم المنتج : ")
+price = input ("ادخل سعر الواحده: ")
+number1 = input ("ادخل عدد المنتج: ")
+result = int(number1) * int(price)
+print ("product name is " +name+ "\nyou bought " +str(number1)+ "\ntotal price is " +str(result))
---
+++ my calculator.py
@@ -0,0 +1,4 @@
+num1 = input ("enter the first no.: ")
+num2 = input ("enter the sec no.: ")
+result = float(num1) + float(num2)
+print ("you got this shit " + str(result))
