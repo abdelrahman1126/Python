179b1d7529f0a0782af71203c2f8801fad58083a
abdelrahman1126 <bodorody@gmail.com>
2026-02-25 17:25:27 +0200

    my calculator.py

--- my calculator.py
+++ my calculator.py
@@ -1,4 +1,4 @@
 num1 = input ("enter the first no.: ")
 num2 = input ("enter the sec no.: ")
 result = float(num1) + float(num2)
-print ("you got this shit " + str(result))
+print ("you got this " + str(result))
