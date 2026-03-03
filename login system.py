c94ef3bf1909287361da139c09e2c6020fbd0d53
abdelrahman1126 <bodorody@gmail.com>
2026-03-03 04:47:35 +0200

    Add files via upload

---
+++ login system.py
@@ -0,0 +1,16 @@
+print ("sign up")
+name1 = input("enter ur name: ")
+password1 = input("enter the password: ")
+attempts = 0
+while attempts < 3:
+    print ("sign in")
+    name2 = input("enter name: ")
+    password2 = input("enter the pass: ")
+    if name1 == name2 and password1 == password2:
+     print("welcome bodo")
+     break
+    else:
+     attempts = attempts + 1
+     print ("wrong pass")
+    if attempts == 3:
+     print ("go away hackkkerrrrr")
